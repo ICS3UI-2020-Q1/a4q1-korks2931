6860383a8945730323dfc8a7bd5514c9fe3e111c
korks2931 <korks2931@wrdsb.ca>
2020-10-02 16:09:42 +0000

    finished a4q1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will count down from 100 by 5 until its gets to the entered number.
+ * @author Sevde
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // prompt the user for an integer 
+    System.out.println("Please enter an integer to count down to ");
+   // declare a variable to store integer
+   int integer = input.nextInt();
+   // countdown
+   System.out.println("Count Down:");
+   // create a counter variable to keep track of what number we are on startint at 100
+   int count = 100;
+   // use a loop to count down by 5
+   while(count >= integer){
+     // tell the user the count 
+   System.out.println( count);
+   count = count - 5;
+
+   }
+
+
+
   }
 }
